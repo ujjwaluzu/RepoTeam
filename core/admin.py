9492a00c5bd3f35ea176d8367c863c8b3149d177
ujjwaluzu <baunthiyalujjwal52@gmail.com>
2026-09-10 01:40:58 +0530

    admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Team, Membership, Project, Issue,Comment 
 # Register your models here.
+admin.site.register(Team)
+admin.site.register(Membership)
+admin.site.register(Project)
+admin.site.register(Issue)
+admin.site.register(Comment)
