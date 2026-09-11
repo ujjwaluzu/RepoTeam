3e8107cff3029fc5236e2bda1fd28c4daee6c996
ujjwaluzu <baunthiyalujjwal52@gmail.com>
2026-09-11 14:57:22 +0530

    basic auth

--- core/views.py
+++ core/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()  # Saves the user to the database
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('login')  # Redirect to your login url
+    else:
+        form = RegistrationForm()
+        
+    return render(request, 'core/register.html', {'form': form})
